07b0456beb12dc8484bafa46d6ee8a83b85d05e3
jeliaaa <julia.aleqsandre@gmail.com>
2023-12-10 00:29:50 +0400

    .

--- src/pages/apartment/Apartment.jsx
+++ src/pages/apartment/Apartment.jsx
@@ -6,31 +6,33 @@ import apartmentImg from '../../assets/apartsImages/apartment.jpg'
 import apartmentsHero from '../../assets/apartsImages/apartmentsHero.jpg'
 import numIcon from '../../assets/icons/numberIcon.png'
 import BackBtn from '../../ReusableComps/backbtn/BackBtn'
+import { useTranslation } from 'react-i18next'
 const Apartment = () => {
     let data = [
-        {id :'1', address: "Chavch1a", sadarbazo: "ravi"},
-        {id :'2', address: "Chavch2a", sadarbazo: "ravi"},
-        {id :'3', address: "Chavch3a", sadarbazo: "ravi"},
-        {id :'4', address: "Chavch4a", sadarbazo: "ravi"},
-        {id :'5', address: "Chavch5a", sadarbazo: "ravi"},
-        {id :'6', address: "Chavch6a", sadarbazo: "ravi"},
-        {id :'7', address: "Chavch7a", sadarbazo: "ravi"},
-        {id :'8', address: "Chavch8a", sadarbazo: "ravi"},
-        {id :'9', address: "Chavch9a", sadarbazo: "ravi"},
-        {id :'10', address: "Chavch10a", sadarbazo: "ravi"},
-        {id :'11', address: "Chavch11a", sadarbazo: "ravi"},
-        {id :'12', address: "Chavch12a", sadarbazo: "ravi"},
-        {id :'13', address: "Chavch13a", sadarbazo: "ravi"},
-        {id :'14', address: "Chavch14a", sadarbazo: "ravi"},
-        {id :'15', address: "Chavch15a", sadarbazo: "ravi"},
-        {id :'16', address: "Chavch16a", sadarbazo: "ravi"},
-        {id :'17', address: "Chavch17a", sadarbazo: "ravi"},
-        {id :'18', address: "Chavch18a", sadarbazo: "ravi"},
-        {id :'19', address: "Chavch19a", sadarbazo: "ravi"},
-        {id :'20', address: "Chavch20a", sadarbazo: "ravi"},
+        {id :'1', address: "Chavch1a", sadarbazo: "1"},
+        {id :'2', address: "Chavch2a", sadarbazo: "1"},
+        {id :'3', address: "Chavch3a", sadarbazo: "1"},
+        {id :'4', address: "Chavch4a", sadarbazo: "1"},
+        {id :'5', address: "Chavch5a", sadarbazo: "1"},
+        {id :'6', address: "Chavch6a", sadarbazo: "1"},
+        {id :'7', address: "Chavch7a", sadarbazo: "1"},
+        {id :'8', address: "Chavch8a", sadarbazo: "2"},
+        {id :'9', address: "Chavch9a", sadarbazo: "2"},
+        {id :'10', address: "Chavch10a", sadarbazo: "2"},
+        {id :'11', address: "Chavch11a", sadarbazo: "2"},
+        {id :'12', address: "Chavch12a", sadarbazo: "2"},
+        {id :'13', address: "Chavch13a", sadarbazo: "2"},
+        {id :'14', address: "Chavch14a", sadarbazo: "2"},
+        {id :'15', address: "Chavch15a", sadarbazo: "2"},
+        {id :'16', address: "Chavch16a", sadarbazo: "3"},
+        {id :'17', address: "Chavch17a", sadarbazo: "3"},
+        {id :'18', address: "Chavch18a", sadarbazo: "3"},
+        {id :'19', address: "Chavch19a", sadarbazo: "3"},
+        {id :'20', address: "Chavch20a", sadarbazo: "3"},
 ]
 
     const { name } = useParams()
+    const { t } = useTranslation();
     const [searchTerm, setSearchTerm] = useState('');
     const [filteredData, setFilteredData] = useState(data);
     const Scroll = require('react-scroll')
@@ -69,7 +71,7 @@ const Apartment = () => {
                         <InputLeftElement pointerEvents='none'>
                             <img width={20} src={numIcon} />
                         </InputLeftElement>
-                        <Input onChange={handleInputChange} type='number' placeholder='შეიყვანეთ ბინის ნომერი' />
+                        <Input onChange={handleInputChange} type='number' placeholder={t('enter')} />
                     </InputGroup>
                 </Stack>
             </div>
@@ -87,10 +89,10 @@ const Apartment = () => {
                         <CardFooter sx={{ backgroundColor: 'black', color: 'white' }}>
                             <Stack>
                                 <div>
-                                    <h4>Apartment N{e.id}</h4>
+                                    <h4>{t('apartment')} N{e.id}</h4>
                                     <div className="hovered">
-                                        <p className='address'>address: {e.address}</p>
-                                        <p className='sadarbazo'>sadarbazo: {e.sadarbazo}</p>
+                                        <p className='address'>{t('address')}: {e.address}</p>
+                                        <p className='sadarbazo'>{t('entrance')} : {e.sadarbazo}</p>
                                     </div>
                                 </div>
                             </Stack>
